64ad28b68b53e038ba33f7a4cb2776691393c460
Alex <aschloth@stud.hs-heilbronn.de>
2022-11-20 13:28:22 +0100

    Added db functionality for shopping list activity

--- app/src/main/java/com/example/communityshopping/DatabaseHelper.kt
+++ app/src/main/java/com/example/communityshopping/DatabaseHelper.kt
@@ -1,25 +1,21 @@
 package com.example.communityshopping
 
+import android.content.ContentValues
 import android.content.Context
+import android.database.Cursor
 import android.database.sqlite.SQLiteDatabase
 import android.database.sqlite.SQLiteOpenHelper
 
 class DatabaseHelper(
     context: Context?,
-    name: String?,
-    factory: SQLiteDatabase.CursorFactory?,
-    version: Int
-) : SQLiteOpenHelper(context, name, factory, version) {
-
-    private val TABLE_SHOPPING_LIST = "shopping_list"
-    private val COLUMN_ID = "_id"
-    private val COLUMN_ITEM = "shopping_item"
+    factory: SQLiteDatabase.CursorFactory?
+) : SQLiteOpenHelper(context, DATABASE_NAME, factory, DATABASE_VERSION) {
 
     override fun onCreate(db: SQLiteDatabase?) {
 
         val query = "CREATE TABLE " + TABLE_SHOPPING_LIST +
                 " (" + COLUMN_ID + " INTEGER PRIMARY KEY AUTOINCREMENT, " +
-                COLUMN_ITEM + " TEXT);"
+                COLUMN_ITEM_NAME + " TEXT);"
         db!!.execSQL(query)
 
         //TODO Create other tables here (to add more tables use db.execSQL(customizedQuery))
@@ -29,11 +25,34 @@ class DatabaseHelper(
         db!!.execSQL("DROP TABLE IF EXISTS " + TABLE_SHOPPING_LIST)
         onCreate(db)
     }
-    private fun add(){
-        val db = this.writableDatabase
-        //TODO debate how to structure database add method e.g. write different add methods for each screen
+    fun getAllTableData(table: Table): Cursor? {
+        if(table == Table.TABLE_SHOPPING_LIST){
+            val db = this.readableDatabase
+            return db.rawQuery("SELECT * FROM " + TABLE_SHOPPING_LIST, null)
+        }
+        else{
+            return null
+            // TODO add other tables here
+        }
     }
-    private fun delete(){
-        //TODO debate how to structure database delete method
+    fun addItem(name: String){
+        val db = this.writableDatabase
+        val values = ContentValues()
+        values.put(COLUMN_ITEM_NAME, name)
+        db.insert(TABLE_SHOPPING_LIST, null, values)
+        db.close()
+        //TODO write different add methods for each screen
+    }
+    fun deleteItem(){
+        //TODO delete Item from database (table shopping list)
+    }
+
+    companion object {
+        private val DATABASE_NAME = "wgShopping.db"
+        private val DATABASE_VERSION = 1
+
+        val TABLE_SHOPPING_LIST = "shopping_list"
+        val COLUMN_ID = "_id"
+        val COLUMN_ITEM_NAME = "shopping_item"
     }
 }
---
+++ app/src/main/java/com/example/communityshopping/Table.kt
@@ -0,0 +1,5 @@
+package com.example.communityshopping
+
+enum class Table {
+    TABLE_SHOPPING_LIST
+}
