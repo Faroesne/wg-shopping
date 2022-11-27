0554bbadbc26c46acb9e992b9e2169954905f18a
Fabian Coenen <80325637+FCoenen22@users.noreply.github.com>
2022-11-27 23:51:28 +0100

    merge conflict

--- app/src/main/java/com/example/communityshopping/welcome/JoinGroupActivity.kt
+++ app/src/main/java/com/example/communityshopping/welcome/JoinGroupActivity.kt
@@ -1,7 +1,6 @@
 package com.example.communityshopping.welcome
 
 import android.Manifest
-import android.annotation.SuppressLint
 import android.bluetooth.BluetoothAdapter
 import android.bluetooth.BluetoothDevice
 import android.content.BroadcastReceiver
@@ -9,14 +8,12 @@ import android.content.Context
 import android.content.Intent
 import android.content.IntentFilter
 import android.content.pm.PackageManager
-import android.os.Build
 import android.os.Bundle
 import android.util.Log
 import android.view.View
 import android.widget.Button
 import android.widget.LinearLayout
 import android.widget.TextView
-import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
 import com.example.communityshopping.R
