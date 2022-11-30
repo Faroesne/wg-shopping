2c11ba7b1a3086dd8c273ca5dd00f55bea71272e
Fabian Coenen <80325637+FCoenen22@users.noreply.github.com>
2022-11-30 12:11:11 +0100

    Username und BluetoothStatus in sharedPreferences

--- app/src/main/java/com/example/communityshopping/welcome/CreateGroupActivity.kt
+++ app/src/main/java/com/example/communityshopping/welcome/CreateGroupActivity.kt
@@ -1,9 +1,11 @@
 package com.example.communityshopping.welcome
 
 import android.annotation.SuppressLint
+import android.content.Context
 import android.content.Intent
 import android.os.Build
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
 import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
@@ -15,6 +17,7 @@ class CreateGroupActivity : AppCompatActivity() {
 
     lateinit var createButton: Button
 
+
     @RequiresApi(Build.VERSION_CODES.M)
     @SuppressLint("SuspiciousIndentation")
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -25,7 +28,18 @@ class CreateGroupActivity : AppCompatActivity() {
     }
 
     private fun createGroup() {
+        saveBluetoothStatus()
         startActivity(Intent(this, MainActivity::class.java))
     }
 
+    private fun saveBluetoothStatus() {
+        val sharedPref = this.getSharedPreferences(
+            getString(R.string.app_preferences), Context.MODE_PRIVATE
+        )
+        with(sharedPref.edit()) {
+            putInt(getString(R.string.bluetooth_setup_status), 1)
+            Log.i("sharedPref", "UserName gespeichert.")
+            apply()
+        }
+    }
 }
--- app/src/main/java/com/example/communityshopping/welcome/WelcomeActivity.kt
+++ app/src/main/java/com/example/communityshopping/welcome/WelcomeActivity.kt
@@ -1,23 +1,24 @@
 package com.example.communityshopping.welcome
 
 import android.annotation.SuppressLint
-import android.bluetooth.BluetoothAdapter
-import android.bluetooth.BluetoothManager
+import android.content.Context
 import android.content.Intent
 import android.os.Build
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
-import android.widget.Toast
+import android.widget.TextView
 import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.app.ActivityCompat
 import com.example.communityshopping.R
 import com.example.communityshopping.communication.BluetoothHelper
+import com.example.communityshopping.mainActivity.MainActivity
 
 class WelcomeActivity : AppCompatActivity() {
 
     lateinit var joinBtn: Button
     lateinit var createButton: Button
+    lateinit var textField: TextView
 
     private lateinit var bluetoothHelper: BluetoothHelper
 
@@ -32,20 +33,48 @@ class WelcomeActivity : AppCompatActivity() {
         createButton = findViewById(R.id.gruppeErstellen)
         joinBtn.setOnClickListener { joinGroup() }
         createButton.setOnClickListener { createGroup() }
+        textField = findViewById(R.id.textFieldName)
 
+        checkForExistingSetup()
         bluetoothHelper = BluetoothHelper(this)
     }
 
+
     private fun joinGroup() {
+        saveUserName()
         startActivity(Intent(this, JoinGroupActivity::class.java))
     }
 
     private fun createGroup() {
+        saveUserName()
         startActivity(Intent(this, CreateGroupActivity::class.java))
     }
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
         bluetoothHelper.onActivityResult(requestCode, resultCode, data)
+    }
+
+    private fun saveUserName() {
+        val sharedPref = this.getSharedPreferences(
+            getString(R.string.app_preferences), Context.MODE_PRIVATE
+        )
+        with(sharedPref.edit()) {
+            putString(getString(R.string.user_name), textField.text.toString())
+            Log.i("sharedPref", "UserName gespeichert.")
+            apply()
+        }
+    }
+
+    private fun checkForExistingSetup() {
+        val sharedPref = this.getSharedPreferences(
+            getString(R.string.app_preferences), Context.MODE_PRIVATE
+        )
+        val bluetoothSetup = sharedPref.getInt(getString(R.string.bluetooth_setup_status), 0)
+        Log.i("sharedPref", bluetoothSetup.toString())
+        if (bluetoothSetup == 1) {
+            Log.i("sharedPref", "Main screen loaded")
+            startActivity(Intent(this, MainActivity::class.java))
+        }
     }
 }
